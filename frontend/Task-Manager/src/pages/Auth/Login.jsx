133ec8ae0c6ce91554ccbf573da8d3b1ba7cb327
Sajidullah Khan <sajiidullahkhan0348@gmail.com>
2025-05-18 16:50:07 +0500

    10th Commit

--- frontend/Task-Manager/src/pages/Auth/Login.jsx
+++ frontend/Task-Manager/src/pages/Auth/Login.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+  return <AuthLayout>Login</AuthLayout>
 }
 
 export default Login
